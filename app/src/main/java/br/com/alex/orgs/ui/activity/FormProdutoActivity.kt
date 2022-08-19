d420246f7504da16b0db5bae710b2d4cf99453e4
Alexander de Oliveira Parreira <41374999+AlexParreira@users.noreply.github.com>
2022-08-19 15:40:30 -0300

    Add New Activity

---
+++ app/src/main/java/br/com/alex/orgs/ui/activity/FormProdutoActivity.kt
@@ -0,0 +1,14 @@
+package br.com.alex.orgs.ui.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import br.com.alex.orgs.R
+
+class FormProdutoActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_form_produto)
+
+    }
+}
