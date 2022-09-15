5efcb47b7bdb9bda8883f984055c7d7eaddbc04e
Alexander de Oliveira Parreira <41374999+AlexParreira@users.noreply.github.com>
2022-09-15 15:02:46 -0300

    Add Format font and image

--- app/src/main/java/br/com/alex/orgs/dao/ProdutoDao.kt
+++ app/src/main/java/br/com/alex/orgs/dao/ProdutoDao.kt
@@ -1,17 +1,25 @@
 package br.com.alex.orgs.dao
 
 import br.com.alex.orgs.model.Produto
+import java.math.BigDecimal
 
 class ProdutoDao {
 
-    fun adicionar(produto: Produto){
+    fun adicionar(produto: Produto) {
         produtos.add(produto)
     }
-    fun buscaTodos() : List<Produto>{
+
+    fun buscaTodos(): List<Produto> {
         return produtos.toList()
     }
 
     companion object {
-        private  val produtos = mutableListOf<Produto>()
+        private val produtos = mutableListOf<Produto>(
+            Produto(
+                nome = "Salada de frutas",
+                descricao = "Laranja, maçãs e uvas",
+                valor = BigDecimal("19.83")
+            )
+        )
     }
 }
